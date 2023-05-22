3f683fa90dc084e95a79d4bbbfc0a8422e994d67
toqawasfi <toqawasfi123@gmail.com>
2023-05-22 06:34:17 +0000

    aloha done

--- api/aloha.py
+++ api/aloha.py
@@ -1,11 +1,13 @@
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
+
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
+        s=self.path
+        print(s)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        text="Hello Toto98"
-        self.wfile.write( text.encode('utf-8'))
+        self.wfile.write('Hello, world!'.encode('utf-8'))
         return
-    
